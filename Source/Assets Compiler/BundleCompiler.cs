b393e6caf7cc8c397f5f441ec731fc1fe1a8aace
Axel Habermaier <axelhabermaier@gmail.com>
2017-08-02 11:58:33 +0200

    Revert "assets embedding"
    This reverts commit 1bb4fcdbca4d03d444d3bea5b2717418edb98320.

--- Source/Assets Compiler/BundleCompiler.cs
+++ Source/Assets Compiler/BundleCompiler.cs
@@ -99,6 +99,7 @@ namespace AssetsCompiler
 			{
 				pakWriter.Write(hash);
 				pakWriter.Write(contentArray.Length);
+				pakWriter.Write(compressedContent.Length);
 				pakWriter.Write(compressedContent);
 			}
 
@@ -120,7 +121,6 @@ namespace AssetsCompiler
 				{
 					var hashBytes = String.Join(", ", hash.Select(b => b.ToString()));
 					writer.AppendLine($"private static readonly Guid Guid = new Guid(new byte[] {{ {hashBytes} }});");
-					writer.AppendLine($"private const string ResourceName = \"OrbsHavoc.{Path.GetFileName(PakFile)}\";");
 					writer.NewLine();
 
 					foreach (var asset in assets)
--- Source/Orbs Havoc/Assets/AssetBundle.cs
+++ Source/Orbs Havoc/Assets/AssetBundle.cs
@@ -25,7 +25,7 @@ namespace OrbsHavoc.Assets
 	using System;
 	using System.IO;
 	using System.IO.Compression;
-	using System.Reflection;
+	using Platform;
 	using Platform.Logging;
 	using Platform.Memory;
 	using Scripting;
@@ -53,7 +53,7 @@ namespace OrbsHavoc.Assets
 		private static void Load()
 		{
 			var start = Clock.GetTime();
-			var assets = Decompress(Assembly.GetExecutingAssembly().GetManifestResourceStream(ResourceName));
+			var assets = Decompress(FileSystem.ReadAllBytes("Assets.pak"));
 
 			LoadAssets(new BufferReader(assets, Endianess.Little));
 			Log.Info($"Asset bundle loaded ({(Clock.GetTime() - start) * 1000:F2}ms).");
@@ -62,24 +62,20 @@ namespace OrbsHavoc.Assets
 		/// <summary>
 		///   Decompresses the compressed content.
 		/// </summary>
-		private static byte[] Decompress(Stream compressedContent)
+		private static byte[] Decompress(byte[] compressedContent)
 		{
+			var buffer = new BufferReader(compressedContent, Endianess.Little);
+
 			// Validate the header
-			if (compressedContent.Length < 16)
+			if (!buffer.CanRead(16))
 				Log.Die("Asset bundle is corrupted: Header information missing.");
 
-			var guid = new byte[16];
-			compressedContent.Read(guid, 0, guid.Length);
-
-			if (Guid != new Guid(guid))
+			if (Guid != new Guid(buffer.ReadByteArray(16)))
 				Log.Die("Asset bundle is corrupted: The header contains an invalid hash.");
 
 			// Decompress the bundle's content
-			var contentLength = new byte[4];
-			compressedContent.Read(contentLength, 0, contentLength.Length);
-			
-			var content = new byte[BitConverter.ToInt32(contentLength, 0)];
-			using (var stream = new GZipStream(compressedContent, CompressionMode.Decompress))
+			var content = new byte[buffer.ReadInt32()];
+			using (var stream = new GZipStream(new MemoryStream(buffer.ReadByteArray()), CompressionMode.Decompress))
 				stream.Read(content, 0, content.Length);
 
 			return content;
