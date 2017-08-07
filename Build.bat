41c055d340dca8e299d62a5f5b27a8fca1251123
Axel Habermaier <axelhabermaier@gmail.com>
2017-08-07 15:11:25 +0200

    packaging is now always performed for Release builds

--- Build.bat
+++ Build.bat
@@ -20,20 +20,13 @@ echo =====================================================================
 "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" "Orbs Havoc.sln" /t:rebuild /p:Configuration=Release /nr:false /nologo /v:minimal /p:Platform=x64
 
 mkdir Binaries
-
-echo =====================================================================
-echo Merging and linking assemblies...
-echo =====================================================================
-cd Build\Release
-copy "Orbs Havoc.exe" "OrbsHavoc.exe"
-"..\..\Dependencies\SharpPak\SharpPak.exe" OrbsHavoc.exe -a
-cd ..\..\Binaries
+cd Binaries
 
 echo =====================================================================
 echo Copying files...
 echo =====================================================================
 copy "..\Build\Release\Assets.pak" 
-copy "..\Build\Release\Output\OrbsHavoc.exe" "Orbs Havoc.exe"
+copy "..\Build\Release\Orbs Havoc.exe"
 copy "..\Build\Release\SDL2.dll"
 
 cd..
